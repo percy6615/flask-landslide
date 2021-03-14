869584cc3fc353593fb787ca43f9a14538c37fd9
Your Name <you@example.com>
2021-03-15 07:30:31 +0800

    init

--- app/model/global_data.py
+++ app/model/global_data.py
@@ -1,20 +1,24 @@
 import json
 import pathlib
-from abc import ABCMeta
 
 from .. import kerasVersion_subFolder, getConfig
-from ..tools.sync_tool import Singletonclass
+from ..tools.sync_tool import  Singletonclass
 
 
 @Singletonclass.singleton
-class GlobalInMem():
-    def __init__(self):
-        self.kerasfilejsondata = self.load(kerasVersion_subFolder + "/data.json")
-        self.keras_version_record = self.load(getConfig.getVersion_dispatch_record())
-        self.device_token = self.load(getConfig.getDispatch_device_token())
+class KerasGlobalInMem:
+    # getInstance
 
-    def load(self, filePath):
-        return json.loads(pathlib.Path(filePath, encoding="utf-8").read_text(encoding='utf-8'))
+    def __init__(self):
+        self.kerasfilejsondata = json.loads(pathlib.Path(kerasVersion_subFolder + "/data.json",
+                                                         encoding="utf-8").read_text(
+            encoding='utf-8'))  # 解析為陣列
+        self.keras_version_record = json.loads(pathlib.Path(getConfig.getVersion_dispatch_record(),
+                                                            encoding="utf-8").read_text(
+            encoding='utf-8'))
+        self.device_token = json.loads(pathlib.Path(getConfig.getDispatch_device_token(),
+                                                    encoding="utf-8").read_text(
+            encoding='utf-8'))
 
     def getkerasfilejsondata(self):
         return self.kerasfilejsondata
@@ -24,5 +28,3 @@ class GlobalInMem():
 
     def getDevice_token_record(self):
         return self.device_token
-
-
